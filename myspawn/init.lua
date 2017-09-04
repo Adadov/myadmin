affb97b6344ab02fa15a91ec80fc29535fe7394d
David OLIVIER <adadov@adadov.net>
2017-09-04 13:49:26 +0200

    Add functions to easy telepot

--- myspawn/init.lua
+++ myspawn/init.lua
@@ -1,10 +1,11 @@
 -- If not using then set the name to nil
 
-local default_respawn = {x=-2293, y=11.5, z=-1515}
+minetest.register_privilege("setspawn", "Lets person set respawn point")
+minetest.register_privilege("setpoint", "Lets person set predefined points")
 
--- Spawn 1
-local s1n = "spawn"
-local s1 = default_respawn
+myspawn = {}
+myspawn.storage = minetest.get_mod_storage()
+minetest.log("[MySpawn] "..dump(myspawn.storage:to_table().fields))
 
 -- Spawn 2
 local s2n = "spawn2"
@@ -23,53 +24,130 @@ local s5n = nil
 local s5 = {x = 0, y = 0, z = 0}
 
 -- Nothing to change past here
+-- Respawn function
 minetest.register_on_respawnplayer(function(player)
-    player:setpos(default_respawn)
+    myspawn.go(player:get_player_name(), myspawn.get("spawn"))
     return true
 end)
 
-myspawn = {}
-
+-- Teleport name to pos
 myspawn.go = function(name, pos)
-	if name ~= nil then
-		if pos ~= nil then
-			local player = minetest.get_player_by_name(name)
-			player:setpos(pos)
-			return true
-		end
+	if name ~= nil and pos ~= nil then
+		local player = minetest.get_player_by_name(name)
+		player:setpos(pos)
+		return true
 	end
 	return false
 end
 
+-- Set registered point
+myspawn.set = function(name, pos)
+	if name ~= nil and pos ~= nil then
+		myspawn.storage:set_string(name, minetest.pos_to_string(pos))
+		return true
+	end
+	return false, "Erreur setting point "..name
+end
+
+-- Get registered point
+myspawn.get = function(name)
+	if name ~= nil then
+		local value = myspawn.storage:get_string(name)
+		return minetest.string_to_pos(value)
+	end
+	return false, "name is nil"
+end
+
+-- Commands
 minetest.register_chatcommand("respawn", {
 	description = "Teleport to spawn point",
 	func = function(name)
-		if myspawn.go(name, default_respawn) then
+		if myspawn.go(name, myspawn.get("spawn")) then
 			return true
 		end
 		return false, "Spawn point error"
 	end,
 })
 
-minetest.register_chatcommand("spawntest", {
-	description = "Teleport to spawn point",
-	func = function(name)
+minetest.register_chatcommand("setspawn", {
+	description = "Set spawn point",
+	privs = "setspawn",
+	func = function(name, txt)
+		local player = minetest.get_player_by_name(name)
+		local pos
+		if txt == "" then
+			pos = player:getpos()
+			txt = minetest.pos_to_string(pos)
+			minetest.log("Text is nil")
+		else
+			pos = minetest.string_to_pos(txt)
+			minetest.log("Text:"..txt)
+		end
+		minetest.chat_send_player(name, "Position of spawn defined to "..txt)
+		myspawn.set("spawn", pos)
+		return true
+	end,
+})
+
+minetest.register_chatcommand("getpoint", {
+	description = "Get registered point",
+	privs = "setpoint",
+	func = function(name, txt)
+		local point = myspawn.get(txt)
+		minetest.chat_send_player(name, "Position "..txt.." defined to "..minetest.pos_to_string(point))
+		return true
+	end,
+})
+
+minetest.register_chatcommand("points", {
+	description = "Get all registered points",
+	privs = "setpoint",
+	func = function(name, txt)
+		local msg = ""
+		for point, value in pairs(myspawn.storage:to_table().fields) do
+			msg = msg.."\n"..point..": "..value
+		end
+			minetest.chat_send_player(name, "Configured points: \n"..msg)
+		return true
+	end,
+})
+
+minetest.register_chatcommand("go", {
+	description = "Teleport to custom point",
+	privs = "server",
+	func = function(name, txt)
+		local spawnpoint = myspawn.get(txt)
 		if myspawn.go(name, spawnpoint) then
+			minetest.chat_send_player(name, "Teleport to "..minetest.pos_to_string(spawnpoint))
 			return true
 		end
 		return false, "Spawn point error "
 	end,
 })
 
-minetest.register_chatcommand("setspawn", {
-	description = "Set spawn point",
-	privs = "server",
+minetest.register_chatcommand("setpoint", {
+	description = "Set teleport point",
+	privs = "setpoint",
 	func = function(name, txt)
+		minetest.chat_send_player(name, "Debug:"..txt)
 		local player = minetest.get_player_by_name(name)
-		minetest.chat_send_player(name, txt)
-		return false, "Spawn point error "
-	end,
+		for point, value in txt:gmatch("([%w_-]+)%s(%S+ %S+ %S+)") do
+			local pos = minetest.string_to_pos(value)
+			minetest.chat_send_player(name, "Position of "..point.." defined to "..value)
+			myspawn.set(point, pos)
+			return true
+		end
+		for point in txt:gmatch("([%w_-]+)") do
+			local pos = player:getpos()
+			local value = minetest.pos_to_string(pos)
+			minetest.chat_send_player(name, "Position of "..point.." defined to "..value)
+			myspawn.set(point, pos)
+			return true
+		end
+		return false, "Test"
+	end
 })
+
 
 --[[
 minetest.register_on_chat_message(function(name, message, playername, player)
