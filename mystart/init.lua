d85d412e5a65ba1510a211c3a66cea246c8c7ace
David OLIVIER <adadov@adadov.net>
2017-09-06 14:51:11 +0200

    Improve first start functions

--- mystart/init.lua
+++ mystart/init.lua
@@ -1,40 +1,38 @@
 
-local servername = "Our Server"
+local servername = minetest.setting_get('server_name')
 
-
-local f = assert(io.open(minetest.get_modpath("mystart").."/rules.txt", "r"))
+local f = assert(io.open(minetest.get_worldpath().."/rules.txt", "r"))
 local the_text = f:read("*all")
-	f:close()
+f:close()
 
-	minetest.chat_send_all("Please tell the admin that the rules file needs to be created")
+minetest.chat_send_all("Please tell the admin that the rules file needs to be created")
 
 minetest.setting_set("default_privs", "shout")
 
-minetest.register_on_joinplayer(function(player)
+minetest.register_on_newplayer(function(player)
 
-	if minetest.get_player_privs(player:get_player_name()).interact ~= true then
+	minetest.log('[MYSTART] New player '..player:get_player_name()..' with privs: '..dump(minetest.get_player_privs(player:get_player_name())))
 
-		minetest.show_formspec(player:get_player_name(), "start_screen",
+	minetest.show_formspec(player:get_player_name(), "start_screen",
 
-			"size[12,9;]"..
-			"bgcolor[#0E1AD8;true]"..
-			"textarea[0.5,0.5;11.5,8;name;;"..the_text.."]"..
-			"label[4,7.5;Do you agree to follow the rules?]"..
-			"button_exit[2,8;3,2;yes;YES]"..
-			"button_exit[7,8;3,2;no;NO]")
-	end
+		"size[12,11;]"..
+		"bgcolor[#0E1AD8;true]"..
+		"textarea[0.5,0.5;11.5,8;name;;"..the_text.."]"..
+		"label[4,7.5;Acceptez-vous les règles?]"..
+		"label[4,8;Do you agree to follow the rules?]"..
+		"button_exit[2,9;6,2;yes;OUI YES]"..
+		"button_exit[7,9;6,2;no;NON NO]")
 
 	minetest.register_on_player_receive_fields(function(player, start_screen, fields)
 		local pname = player:get_player_name()
+		local privs=minetest.get_player_privs(pname)
 
 		if fields["yes"] then
-
-		local privs=minetest.get_player_privs(pname)
 
 			privs.shout=true
 			privs.interact=true
 			privs.home=true
-			privs.fast=false
+			--privs.fast=false
 			minetest.set_player_privs(pname,privs)
 
 			return true
